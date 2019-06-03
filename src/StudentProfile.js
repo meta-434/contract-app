0ec6cf4a0d97e4c8ae25083378d69aeded3df166
Anne Williams <mawilliams098@gmail.com>
2019-06-03 11:21:36 -0400

    added material-ui for button

--- src/StudentProfile.js
+++ src/StudentProfile.js
@@ -1,13 +1,35 @@
 import React, { Component } from 'react'
+import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
 
-export class StudentProfile extends Component {
-    render() {
+// material UI setup
+const useStyles = makeStyles(theme => ({
+    button: {
+      margin: theme.spacing(1),
+    },
+    input: {
+      display: 'none',
+    },
+  }));
+
+
+function StudentProfile(props) {
+    const classes = useStyles();
+  
         return (
             <div>
-                
+                {/* Only Available if logged in */}
+                <Button variant="contained" color="primary" className={classes.button}>
+                    Edit Profile
+                </Button>
+
+                {/* Dummy Data  */}
+                <h1>Profile Info</h1>
+
+                <h1>Current Contracts</h1>
             </div>
-        )
-    }
+        )     
+    
 }
 
 export default StudentProfile
