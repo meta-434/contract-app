c6ec90d0099e26a69599a9c575094d1473954f49
Anne Williams <mawilliams098@gmail.com>
2019-06-03 10:37:47 -0400

    branch setup

---
+++ src/StudentProfile.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class StudentProfile extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default StudentProfile
