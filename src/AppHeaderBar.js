ec6bc055b5cbc9742eecd202017f5a303a1ec483
meta-434 <ach1@vt.edu>
2019-06-03 15:39:05 -0400

    Homepage WIP
    navbar works 
    menus won't click off and need to be consolidated
    submit and link buttons need to be styled.

--- src/AppHeaderBar.js
+++ src/AppHeaderBar.js
@@ -1,37 +1,90 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
-import Button from "@material-ui/core/Button";
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
+import Chip from "@material-ui/core/Chip";
+import Avatar from "@material-ui/core/Avatar";
+import Tooltip from "@material-ui/core/Tooltip";
+import Menu from "@material-ui/core/Menu";
+import MenuItem from "@material-ui/core/MenuItem";
 
 const useStyles = makeStyles(theme => ({
   root: {
-    flexGrow: 1,
+    flexGrow: 1
   },
   menuButton: {
-    marginRight: theme.spacing(2),
+    marginRight: theme.spacing(2)
   },
   title: {
-    flexGrow: 1,
-  },
+    flexGrow: 1
+  }
 }));
 
 function DenseAppBar() {
+  function handleDelete() {
+    console.log("This will log you out.");
+  }
+
+ function handleMenuChoice(e) {
+    console.log(e.target.id)
+    console.log("This will eventually redirect to unique pages.");
+  }
+
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  function handleClick (e) {
+    console.log("the menu has been pressed");
+    setAnchorEl(e.currentTarget);
+  }
+
+  function handleClose() {
+    setAnchorEl(null);
+  }
+
   const classes = useStyles();
   return (
     <div className={classes.root}>
       <AppBar position="static">
         <Toolbar variant="dense">
-          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="Menu">
+          <IconButton
+            aria-haspopup="true"
+            edge="start"
+            className={classes.menuButton}
+            color="inherit"
+            aria-label="Menu"
+            onClick={handleClick}
+          >
             <MenuIcon />
+            <Menu
+              id="simple-menu"
+              anchorEl={anchorEl}
+              keepMounted
+              open={Boolean(anchorEl)}
+              onClose={handleClose}
+            >
+              <MenuItem onClick={handleMenuChoice}>Profile</MenuItem>
+              <MenuItem onClick={handleMenuChoice}>Marketplace</MenuItem>
+              <MenuItem onClick={handleMenuChoice}>Contracts</MenuItem>
+            </Menu>
           </IconButton>
           <Typography variant="h6" color="inherit" className={classes.title}>
             RevTek Internships
           </Typography>
-          <Button color="inherit">Login</Button>
+          <Tooltip title="Profile Information">
+            <Chip
+              size="small"
+              avatar={<Avatar>SS</Avatar>}
+              label="Sample Student"
+              clickable
+              className={classes.chip}
+              onClick={handleClick} // visit my profile
+              color="primary"
+              onDelete={handleDelete} // sign out
+            />
+          </Tooltip>
         </Toolbar>
       </AppBar>
     </div>
--- src/Home.js
+++ src/Home.js
@@ -1,72 +1,158 @@
 import React, { Component } from "react";
 import AppHeaderBar from "./AppHeaderBar";
-import { Container, Typography, Card } from "@material-ui/core";
-import { makeStyles } from "@material-ui/core";
-import ExpansionPanel from '@material-ui/core/ExpansionPanel';
-import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
-import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
-
-const useStyles = makeStyles(theme => ({
-  root: {
-    padding: theme.spacing(3, 2)
-  },
-  card: {
-    minWidth: 275
-  },
-}));
+import { Container, Typography, Card, Button, Grid } from "@material-ui/core";
+import ExpansionPanel from "@material-ui/core/ExpansionPanel";
+import ExpansionPanelSummary from "@material-ui/core/ExpansionPanelSummary";
+import ExpansionPanelDetails from "@material-ui/core/ExpansionPanelDetails";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import CloudUploadIcon from "@material-ui/icons/CloudUpload";
 
 class Home extends Component {
-  render() {
-    //const classes = this.useStyles;
+  constructor(props) {
+    super(props);
 
+    this.state = {};
+  }
+
+  render() {
     return (
       <div>
         <AppHeaderBar />
-        
+
         <Container maxWidth="md">
-        <div>
-          <p>
-            Title Placeholder
-          </p>
-        </div>
-        <ExpansionPanel>
-          <ExpansionPanelSummary
-            expandIcon={<ExpandMoreIcon />}
-            aria-controls="panel1a-content"
-            id="panel1a-header"
-          >
-            <Typography >
-              Expansion Panel 1
-            </Typography>
-          </ExpansionPanelSummary>
-          <ExpansionPanelDetails>
-            <Typography>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-              Suspendisse malesuada lacus ex, sit amet blandit leo lobortis
-              eget.
-            </Typography>
-          </ExpansionPanelDetails>
-        </ExpansionPanel>
-        <ExpansionPanel>
-          <ExpansionPanelSummary
-            expandIcon={<ExpandMoreIcon />}
-            aria-controls="panel2a-content"
-            id="panel2a-header"
-          >
-            <Typography >
-              Expansion Panel 2
-            </Typography>
-          </ExpansionPanelSummary>
-          <ExpansionPanelDetails>
-            <Typography>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-              Suspendisse malesuada lacus ex, sit amet blandit leo lobortis
-              eget.
-            </Typography>
-          </ExpansionPanelDetails>
-          
-        </ExpansionPanel>
+          <div>
+            <h3>Task Board</h3>
+          </div>
+          <Card>
+            <ExpansionPanel>
+              <ExpansionPanelSummary
+                expandIcon={<ExpandMoreIcon />}
+                aria-controls="panel1a-content"
+                id="panel1a-header"
+              >
+                <Typography>Sample Task 1</Typography>
+              </ExpansionPanelSummary>
+              <ExpansionPanelDetails>
+                <Typography>The task description can be short, or-</Typography>
+              </ExpansionPanelDetails>
+            </ExpansionPanel>
+          </Card>
+          <p>{"\n"}</p>
+          <Card>
+            <ExpansionPanel>
+              <ExpansionPanelSummary
+                expandIcon={<ExpandMoreIcon />}
+                aria-controls="panel1a-content"
+                id="panel1a-header"
+              >
+                <Typography>Sample Task 2</Typography>
+              </ExpansionPanelSummary>
+              <ExpansionPanelDetails>
+                <Grid container spacing={3}>
+                  <Grid item xs={12}>
+                    <Typography>
+                      It can be very long. Lorem ipsum dolor sit amet,
+                      consectetur adipiscing elit. Suspendisse malesuada lacus
+                      ex, sit amet blandit leo lobortis eget. Lorem ipsum dolor
+                      sit amet, consectetur adipiscing elit. Suspendisse
+                      malesuada lacus ex, sit amet blandit leo lobortis eget.
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                      Suspendisse malesuada lacus ex, sit amet blandit leo
+                      lobortis eget. Lorem ipsum dolor sit amet, consectetur
+                      adipiscing elit. Suspendisse malesuada lacus ex, sit amet
+                      blandit leo lobortis eget. Lorem ipsum dolor sit amet,
+                      consectetur adipiscing elit. Suspendisse malesuada lacus
+                      ex, sit amet blandit leo lobortis eget. Lorem ipsum dolor
+                      sit amet, consectetur adipiscing elit. Suspendisse
+                      malesuada lacus ex, sit amet blandit leo lobortis eget.
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                      Suspendisse malesuada lacus ex, sit amet blandit leo
+                      lobortis eget.
+                    </Typography>
+                  </Grid>
+                  <Grid item xs={3}>
+                    <label htmlFor="outlined-button-file">
+                      <Button
+                        variant="contained"
+                        color="default"
+                      >
+                        Reply
+                        <CloudUploadIcon />
+                      </Button>
+                    </label>
+
+                    <label htmlFor="outlined-button-file">
+                      <Button
+                        variant="contained"
+                        color="default"
+                      >
+                        Upload
+                        <CloudUploadIcon />
+                      </Button>
+                    </label>
+                  </Grid>
+                </Grid>
+              </ExpansionPanelDetails>
+            </ExpansionPanel>
+          </Card>
+          <p>{"\n"}</p>
+          <Card>
+            <ExpansionPanel>
+              <ExpansionPanelSummary
+                expandIcon={<ExpandMoreIcon />}
+                aria-controls="panel1a-content"
+                id="panel1a-header"
+              >
+                <Typography>Sample Task 3</Typography>
+              </ExpansionPanelSummary>
+              <ExpansionPanelDetails>
+                <Grid container spacing={3}>
+                  <Grid item xs={12}>
+                    <Typography>
+                      It can be very long. Lorem ipsum dolor sit amet,
+                      consectetur adipiscing elit. Suspendisse malesuada lacus
+                      ex, sit amet blandit leo lobortis eget. Lorem ipsum dolor
+                      sit amet, consectetur adipiscing elit. Suspendisse
+                      malesuada lacus ex, sit amet blandit leo lobortis eget.
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                      Suspendisse malesuada lacus ex, sit amet blandit leo
+                      lobortis eget. Lorem ipsum dolor sit amet, consectetur
+                      adipiscing elit. Suspendisse malesuada lacus ex, sit amet
+                      blandit leo lobortis eget. Lorem ipsum dolor sit amet,
+                      consectetur adipiscing elit. Suspendisse malesuada lacus
+                      ex, sit amet blandit leo lobortis eget. Lorem ipsum dolor
+                      sit amet, consectetur adipiscing elit. Suspendisse
+                      malesuada lacus ex, sit amet blandit leo lobortis eget.
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                      Suspendisse malesuada lacus ex, sit amet blandit leo
+                      lobortis eget.
+                    </Typography>
+                  </Grid>
+                  <Grid item xs={3}>
+                    <label htmlFor="outlined-button-file">
+                      <Button
+                        variant="contained"
+                        color="default"
+                      >
+                        Reply
+                        <CloudUploadIcon />
+                      </Button>
+                    </label>
+                    
+                    <label htmlFor="outlined-button-file">
+                      <Button
+                        variant="contained"
+                        color="default"
+                      >
+                        Upload
+                        <CloudUploadIcon />
+                      </Button>
+                    </label>
+                  </Grid>
+                </Grid>
+              </ExpansionPanelDetails>
+            </ExpansionPanel>
+          </Card>
         </Container>
       </div>
     );
