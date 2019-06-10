f11aaf35a58c11a71241cd0a2b876bf41237afd5
Anne Williams <mawilliams098@gmail.com>
2019-06-10 16:25:35 -0400

    better basic card

--- src/StudentCard.js
+++ src/StudentCard.js
@@ -1,47 +1,43 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 
 const useStyles = makeStyles({
-  card: {
-    minWidth: 275,
-  },
-  bullet: {
-    display: 'inline-block',
-    margin: '0 2px',
-    transform: 'scale(0.8)',
-  },
-  title: {
-    fontSize: 14,
-  },
-  pos: {
-    marginBottom: 12,
-  },
+    card: {
+      maxWidth: 345,
+    },
+    media: {
+      height: 200,
+    },
 });
 
-
-function StudentCard(props){
+export default function StudentCard() {
     const classes = useStyles();
-    const bull = <span className={classes.bullet}>•</span>;
-
+  
     return (
-        <Card className={classes.card}>
-            <CardContent>
-                <Typography className={classes.title} color="textSecondary" gutterBottom>
-                Word of the Day
-                </Typography>
-            </CardContent>
-            <CardActions>
-            </CardActions>
-        </Card>
-
-    )
-
-
-}
-
-export default StudentCard
+      <Card className={classes.card}>
+        <CardActionArea>
+          <CardMedia
+            className={classes.media}
+            image="https://www.placecage.com/200/300"
+            title="Contemplative Reptile"
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="h2">
+              Student Name
+            </Typography>
+            <Typography variant="body2" color="textSecondary" component="p">
+              Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
+              across all continents except Antarctica
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+      </Card>
+    );
+  }
