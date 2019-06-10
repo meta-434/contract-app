e4e06b1c3c78ea776797b4e7b3e0d819698a1c68
meta-434 <ach1@vt.edu>
2019-06-10 17:11:57 -0400

    Login Fix

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
-import React from 'react';
-import './App.css';
-import CompanyProfile from './CompanyProfile.js';
+import React from "react";
+import "./App.css";
+import CompanyProfile from "./CompanyProfile.js";
 // import StudentProfile from './StudentProfile';
 // import StudentBid from './StudentBid';
 // import StudentContracts from './StudentContracts';
-import StudentCard from './StudentCard.js'
-import Home from "./Home.js"
+import StudentCard from "./StudentCard.js";
+import Home from "./Home.js";
+import Login from "./Login";
+import NewUser from "./NewUser";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 function App() {
   return (
-     
     <div>
-     <Home />
-      {/*<CompanyProfile/>*/}
+      <Router>
+        <Route exact path="/" component={Login} />
+        <Route exact path="/NewUser" component={NewUser} />
+      </Router>
+      {/* <Home /> */}
     </div>
   );
 }
