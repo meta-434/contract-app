42c94b77ac870a7b6b9cb58ee28938c00b0936f5
meta-434 <ach1@vt.edu>
2019-06-13 11:24:48 -0400

    tabling functional
    table finally fucking works.

--- src/TestTable.js
+++ src/TestTable.js
@@ -1,4 +1,4 @@
-import React, { Component, useState } from "react";
+import React, { Component } from "react";
 //import firebase connection info
 import firebase from "./firebase";
 //material-ui
@@ -22,127 +22,123 @@ const useStyles = makeStyles(theme => ({
   }
 }));
 
-function createData(issuer, bid_close, con_start, con_end, details, con_id) {
-  return { issuer, bid_close, con_start, con_end, details, con_id };
-}
+class SimpleTable extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      rows: []
+    };
+  }
 
-const rows = [
- parseData
-];
+  // cnx() {
+  //   const db = firebase.database().ref("users");
+  //   db.on("value", this.gotData, this.errData);
+  // }
 
-function cnx() {
-  const db = firebase.database().ref("users");
-  db.on("value", gotData, errData);
-}
+  createData(issuer, title, details, bid_close, con_start, con_end) {
+    return { issuer, title, details, bid_close, con_start, con_end };
+  }
 
-function gotData(data) {
-  console.log("connection established!");
-  let comp = data.val().company;
-  parseData(comp);
-}
+  cnx = () => {
+    const db = firebase.database().ref("users/company");
+    db.once("value", snapshot => {
+      this.setState({ dbValue: snapshot.toJSON() });
+    });
+  };
 
-function parseData(target) {
-  let c = target;
-  console.log("c= ", c);
-  let names = [];
-  let contractObjects = [];
-  let results = [];
+  gotData() {
+    console.log("connection established!");
+  }
 
-  Object.keys(c).map(item => {
-    names.push(c[item].name);
-    if (c[item].contracts !== undefined) {
-      contractObjects.push(c[item].contracts);
-    } else {
-      contractObjects.push("");
+  errData(err) {
+    alert("error fetching data");
+    console.log(err);
+  }
+
+  parseData() {
+    let c = this.state.dbValue;
+    let contractObjects = [];
+    let result = [];
+
+    console.log("c= ", c);
+    Object.keys(c).map(item => {
+      if (c[item].contracts !== undefined) {
+        contractObjects.push(c[item].contracts);
+      } else {
+        contractObjects.push("");
+      }
+    });
+
+    for (let i = 0; i < Object.keys(contractObjects).length; i++) {
+      console.log("gets to outer loops");
+      if (contractObjects[i] !== "") {
+        Object.keys(contractObjects[i]).map(k => {
+          console.log("gets to push");
+          this.state.rows.push(
+            this.createData(
+              contractObjects[i][k][0],
+              contractObjects[i][k][1],
+              contractObjects[i][k][2],
+              contractObjects[i][k][3],
+              contractObjects[i][k][4],
+              contractObjects[i][k][5]
+            )
+          );
+        });
+      }
     }
-  });
-
-  console.log("names= ", names, " contracts= ", contractObjects);
-
-  for (let i = 0; i < names.length; i++) {
-    console.log(contractObjects)
-    results.push(createData(names[i]))
-  }
-  for (let j=0; j < Object.keys(contractObjects).length; j++) {
-    console.log(j)
   }
 
-  console.log("pushtest results: ", results)
-  //     {Object.keys(c).map(item => {
-  //       return (
-  //         <div>
-  //           <li key="name">name: {c[item].name}</li>
-  //           <li key="uid">test item {c[item].uid}</li>
-  //           <li key="contracts">
-  //             contracts:{" "}
-  //             {c[item].contracts !== undefined
-  //               ? Object.keys(c[item].contracts).map(con => {
-  //                   return (
-  //                     <ul>
-  //                       <li>
-  //                         contract name: {c[item].contracts[con].title},
-  //                         details: {c[item].contracts[con].details}{" "}
-  //                       </li>
-  //                     </ul>
-  //                   );
-  //                 })
-  //               : null}
-  //           </li>
-  //           <p>
-  //             {c[item].contracts !== undefined
-  //               ? console.log("data retrieved successfully!")
-  //               : null}
-  //           </p>
-  //         </div>
-  //       );
-  //     })}
-
-  // );
-}
-
-function errData(err) {
-  alert("error fetching data");
-  console.log(err);
-}
-
-function SimpleTable() {
-  cnx();
-
-  const classes = useStyles();
-
-  return (
-    <div>
-      {/* {this.state.companies !== null ? this.parseData() : <h3>Loading...</h3>} */}
-      <Paper className={classes.root}>
-        <Table className={classes.table}>
+  makeTable() {
+    return (
+      <Paper>
+        <Table>
           <TableHead>
             <TableRow>
               <TableCell>Contract&nbsp;Issuer</TableCell>
+              <TableCell align="right">Contract&nbsp;Name</TableCell>
+              <TableCell align="right">Details</TableCell>
               <TableCell align="right">Bid&nbsp;Close&nbsp;Date</TableCell>
               <TableCell align="right">Contract&nbsp;Start&nbsp;Date</TableCell>
               <TableCell align="right">Contract&nbsp;End&nbsp;Date</TableCell>
-              <TableCell align="right">Details</TableCell>
-              <TableCell align="right">Contract&nbsp;ID</TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
-            {rows.map(row => (
+            {this.state.rows.map(row => (
               <TableRow key={row.name}>
                 <TableCell component="th" scope="row">
                   {row.issuer}
                 </TableCell>
+                <TableCell align="right">{row.title}</TableCell>
+                <TableCell align="right">{row.details}</TableCell>
                 <TableCell align="right">{row.bid_close}</TableCell>
                 <TableCell align="right">{row.con_start}</TableCell>
                 <TableCell align="right">{row.con_end}</TableCell>
-                <TableCell align="right">{row.details}</TableCell>
-                <TableCell align="right">{row.con_id}</TableCell>
               </TableRow>
             ))}
           </TableBody>
         </Table>
       </Paper>
-    </div>
-  );
+    );
+  }
+
+  render() {
+    if (this.state.dbValue == undefined) {
+      this.cnx();
+    } else {
+      this.parseData();
+    }
+
+    return (
+      <div>
+        {this.state.dbValue === undefined && this.state.rows !== [] ? (
+          <p>EMPTY</p>
+        ) : (
+          this.makeTable()
+        )}
+      </div>
+    );
+  }
 }
 
 export default SimpleTable;
