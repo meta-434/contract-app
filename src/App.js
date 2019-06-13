dbf76ae30a8cd261ed899f7a423bca947c27f061
Anne Williams <mawilliams098@gmail.com>
2019-06-13 09:49:34 -0400

    started implementing react-admin and firebase add on

--- src/App.js
+++ src/App.js
@@ -10,14 +10,35 @@ import Login from "./Login";
 import NewUser from "./NewUser";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
+import { Admin, Resource, ListGuesser } from 'react-admin';
+import { FirebaseDataProvider } from 'react-admin-firebase';
+
+const config = {
+  apiKey: "AIzaSyDn8cvkD7isdWFDlyXXkprCcAF2-R5nb_w",
+  authDomain: "contracts-data.firebaseapp.com",
+  databaseURL: "https://contracts-data.firebaseio.com",
+  projectId: "contracts-data",
+  storageBucket: "contracts-data.appspot.com",
+  messagingSenderId: "235487946894",
+  appId: "1:235487946894:web:9933b1d6a8a97542"
+};
+
+const dataProvider = FirebaseDataProvider(config);
+
 function App() {
   return (
     <div>
-      <Router>
+      {/* <Router>
         <Route exact path="/" component={Login} />
         <Route exact path="/NewUser" component={NewUser} />
-      </Router>
+      </Router>  */}
+
       {/* <Home /> */}
+
+      <Admin dataProvider={dataProvider} >
+        <Resource name="users" list={ListGuesser} />
+      </Admin>
+      
     </div>
   );
 }
